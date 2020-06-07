9cf7fac8ae4f9f19d106a46b122eb60860072372
Jacek Podkanski <ruby.object@googlemail.com>
2020-06-07 22:56:36 +0100

    switched from Int to Char

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,8 @@ import Lib
 
 import Debug.Trace
 
--- dat :: String
--- dat = "123+45*67"
+dat :: String
+dat = "123+45*67"
 
 -- dat :: [Int]
 -- dat = [10,20,30,40,50]
@@ -13,30 +13,33 @@ import Debug.Trace
 nth :: Int -> [a] -> a
 nth n l = head (drop n l)
 
-parse :: [Int] -> [Int]
+parse :: [Char] -> [Char]
 parse [] = [nonval]
 parse acc =
   let h =  (trace ("ZZZZZZ " ++ show acc)) (head acc)
   in
-    if  even h
-    then [h + 1] ++ (drop 1 acc)
+    if  False
+    then [h ] ++ (drop 1 acc)
     else [nonval] ++ acc
 
-nonval :: Int
-nonval = (-1)
+nonval :: Char
+nonval = 'Z'
 
-none :: [Int] -> Bool
+none :: [Char] -> Bool
 none acc = (length acc >= 1) && (head acc) == nonval
 
-eat :: Int -> [Int] -> [Int]
+eat :: Int -> [Char] -> [Char]
 eat n acc =
   if (n > (length dat))
   then acc
   else (if (none acc)
-        then eat (1 + n) ((if (n == (length dat))
-                           then [-999]
-                           else [nth n dat]) ++ (drop 1 acc))
+        then eat (1 + n) (foo n acc)
         else eat n (parse acc))
+
+foo n acc =
+  ((if (n == (length dat))
+                           then ['E']
+                           else [dat !! n]) ++ (drop 1 acc))
 
 main :: IO ()
 main = do
