c887ccb7d3c023dd02ef5a458beb0b8574215d50
Jacek Podkanski <ruby.object@googlemail.com>
2020-06-08 01:30:49 +0100

    types

--- app/Main.hs
+++ app/Main.hs
@@ -2,44 +2,47 @@ module Main where
 
 import Lib
 
-import Debug.Trace
+-- import Debug.Trace
+
+type TypeName = String
+
+data Foo = C Char | D String | E Int | F (String, Foo) deriving (Eq, Show)
+
 
 dat :: String
 dat = "123+45*67"
 
--- dat :: [Int]
--- dat = [10,20,30,40,50]
-
 nth :: Int -> [a] -> a
 nth n l = head (drop n l)
 
-parse :: [Char] -> [Char]
+parse :: [Foo] -> [Foo]
 parse [] = [nonval]
 parse acc =
-  let h =  (trace ("ZZZZZZ " ++ show acc)) (head acc)
+  let h = (head acc)
   in
     if  False
     then [h ] ++ (drop 1 acc)
     else [nonval] ++ acc
 
-nonval :: Char
-nonval = 'Z'
+nonval :: Foo
+nonval =  C 'Z'
 
-none :: [Char] -> Bool
+none :: [Foo] -> Bool
 none acc = (length acc >= 1) && (head acc) == nonval
 
-eat :: Int -> [Char] -> [Char]
+eat :: Int -> [Foo] -> [Foo]
 eat n acc =
   if (n > (length dat))
   then acc
   else (if (none acc)
         then eat (1 + n) (foo n acc)
-        else eat n (parse acc))
+        else eat n       (parse acc))
 
+foo :: Int -> [Foo] -> [Foo]
 foo n acc =
-  ((if (n == (length dat))
-                           then ['E']
-                           else [dat !! n]) ++ (drop 1 acc))
+  (if (n == (length dat))
+   then [C 'E']
+   else [C (dat !! n)]) ++ (drop 1 acc)
 
 main :: IO ()
 main = do
