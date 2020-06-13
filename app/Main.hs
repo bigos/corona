54beec5b65c9c21dfdedfbeb2aee0bf980f9ae66
Jacek Podkanski <ruby.object@googlemail.com>
2020-06-13 18:10:29 +0100

    debugging

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,29 @@
 module Main where
 
-import Lib
-
 import Debug.Trace
+import Data.Char
+
+import Lib
 
 type TypeName = String
 
-data Foo = EndVal | NonVal | C Char | D String | E Int | F (String, Foo) deriving (Eq, Show)
+data Foo = EndVal | NonVal | C Char | S String | D Int | Node (Foo) deriving (Eq, Show)
+
+-- testing function
+qqq :: Foo -> String
+qqq a =
+  case a of
+    EndVal -> "this is END val"
+    NonVal -> "this is NON val"
+    C c    -> "this is C Char "   ++ show c
+    S s    -> "this is S String " ++ show s
+    D i    -> "this is D Int "    ++ show i
+    Node n -> "this is Node "     ++ show n
+
+
+
+
+
 
 dat :: String
 dat = "123+45*67"
@@ -14,32 +31,38 @@ dat = "123+45*67"
 nth :: Int -> [a] -> a
 nth n l = head (drop n l)
 
-parse :: [Foo] -> [Foo]
-parse [] = [NonVal]
-parse acc =
-  let h = (head acc)
-  in
-    if  False
-    then [h ] ++ (drop 1 acc)
-    else [NonVal] ++ acc
+nextOrEndVal :: Int -> [Foo]
+nextOrEndVal n =
+  if n == (length dat)
+  then [EndVal]
+  else [C (dat !! n)]
 
 none :: [Foo] -> Bool
-none [] = False
+none [] = trace ("first el of " ++ show dat)  False
 none acc = head acc == NonVal
 
 eat :: Int -> [Foo] -> [Foo]
 eat n acc =
-  if (trace (show acc) (n > (length dat)))
+  if trace ("acc is === " ++ show acc) $ (n > (length dat))
   then acc
-  else (if (none acc)
-        then eat (1 + n) (foo n acc)
-        else eat n       (parse acc))
+  else eat2
+  where
+    eat2 = if (none acc)
+           then trace ("TTT " ++ show acc) $ eat (1 + n) (nextOrEndVal n) ++ (drop 1 acc)
+           else trace ("EEE " ++ show acc) $ eat n       (parse acc)
 
-foo :: Int -> [Foo] -> [Foo]
-foo n acc =
-  (if (n == (length dat))
-   then [EndVal]
-   else [C (dat !! n)]) ++ (drop 1 acc)
+parse :: [Foo] -> [Foo]
+parse [] = [NonVal]
+parse acc =
+  let
+    h = head acc
+    res = case h of
+            C c -> (if (isDigit c)
+                    then [Node (D (digitToInt c)) ] ++ (drop 1 acc)
+                    else [Node (C c)] ++ (drop 1 acc))
+            _   -> [NonVal] ++ acc
+  in
+    trace ("res is " ++ show res) $ res
 
 main :: IO ()
 main = do
